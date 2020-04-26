9b4273cccdb4760aaa3d11b5b2d3a99b5472bb6b
Enderson Cezano <endersoncezano@yahoo.com.br>
2020-04-25 22:25:12 -0300

    tentando alterar uma constante

--- RewriteConstString/Program.cs
+++ RewriteConstString/Program.cs
@@ -4,9 +4,33 @@ namespace RewriteConstString
 {
     class Program
     {
-        static void Main(string[] args)
+        // declare a const string - can we modify it? 
+        static string sentence = "The quick brown fox jumped over the lazy dog";
+
+        // force unsafe method
+        static unsafe void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            fixed (char* pSentence = sentence)
+            {
+                // report initial state
+                Console.WriteLine($"The sentence is: {sentence}");
+                Console.WriteLine($"The address of the sentence is: {(IntPtr)pSentence}");
+
+                // modify the string
+                char* p = pSentence;
+                for (var i = 0; i < sentence.Length; i++)
+                {
+                    *p = '*';
+                    p++;
+                }
+
+                // report final state - don't resolve with const, just with static
+                Console.WriteLine($"The sentence is: {sentence}");
+                Console.WriteLine($"The address of the sentence is: {(IntPtr)pSentence}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
