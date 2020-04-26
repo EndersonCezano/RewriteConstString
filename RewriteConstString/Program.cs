c62d4dcaec95fbc5bb68323d19cc72ec6580b1dd
Enderson Cezano <endersoncezano@yahoo.com.br>
2020-04-25 21:02:13 -0300

    Add project files.

---
+++ RewriteConstString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RewriteConstString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
